33dc2ccf42dc79578fb1d7f6d99bf969776d7ebc
Estebann1503 <144995626+Estebann1503@users.noreply.github.com>
2024-11-04 14:11:54 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        intro: 'Welcome to my Vue template',
+      }
+    },
+    methods: {
+      myMethod() {
+        // Eksempel på metode
+      },
+    },
+    computed: {
+      myComputed() {
+        return '' // Eksempel på computed property
+      },
+    }
+  });
+  
